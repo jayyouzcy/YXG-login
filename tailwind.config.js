4851fa440cce169ee6ee5c6ff9c496a45037c748
KiriRainCat <2287990465@qq.com>
2023-07-24 18:17:47 +0800

    🐳 chore: 添加微软雅黑字体class

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        yahei: 'Microsoft Yahei',
+      },
+    },
   },
   plugins: [],
 }
