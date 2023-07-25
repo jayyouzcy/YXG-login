0861f246ecc678bfc5dcdb61f1506dcd5329e97d
KiriRainCat <2287990465@qq.com>
2023-07-25 11:33:30 +0800

    ✨ feat: 登录密码页完成

--- src/components/auth/PasswordModule.vue
+++ src/components/auth/PasswordModule.vue
@@ -3,15 +3,20 @@
     <i-ep-close-bold @click="$router.push({ name: 'auth-index' })" class="ml-4 mt-4 cursor-pointer text-2xl" />
     <div class="text-center">
       <img class="mx-auto mt-1 h-12 w-40 object-cover" src="@/assets/common/GYX_gov.png" />
-      <div class="mt-4 text-2xl font-bold">输入你的手机号码或YXG ID</div>
-      <div class="mx-[12%] mt-1 text-sm">
-        你的账户存在异常登录活动。为了保证你的账户安全，请输入你的手机号码或YXG ID以确认你的身份。
-      </div>
-      <div class="animated-input mx-auto mt-4">
+      <div class="mt-4 text-3xl font-bold">输入你的密码</div>
+      <div class="animated-input mx-auto mt-6">
         <input v-model="identification" type="text" required disabled />
         <label>手机号码或YXG ID</label>
       </div>
-      <button class="btn ml-2 mt-56 bg-blue-700 text-base font-bold text-white">下一步</button>
+      <div class="animated-input mx-auto mt-4">
+        <input v-model="password" type="password" required />
+        <label>密码</label>
+      </div>
+      <div class="hyperlink ml-[5.4rem] mt-1 text-left text-xs">忘记密码？</div>
+      <button class="btn ml-2 mt-36 bg-blue-700 text-base font-bold text-white">登录</button>
+      <div class="mt-2">
+        还没有账号？<span @click="$router.push({ name: 'auth-register' })" class="hyperlink">注册</span>
+      </div>
     </div>
   </div>
 </template>
@@ -20,7 +25,7 @@
 import router from '@/router'
 
 const identification = router.currentRoute.value.query.identification
-console.log(identification)
+const password = ref('')
 </script>
 
 <style scoped>
