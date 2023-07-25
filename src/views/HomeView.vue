0fc4a04e2e842e11cd247b78f051080074cefbfb
KiriRainCat <2287990465@qq.com>
2023-07-25 13:09:07 +0800

    ✨ feat(主页): 临时加个按钮

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="mt-80 text-center text-2xl font-bold">HomePage</div>
+  <div class="text-center">
+    <div class="mb-4 mt-80 text-center text-2xl font-bold">HomePage</div>
+    <el-button @click="$router.push({ name: 'auth-index' })">验证页</el-button>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
