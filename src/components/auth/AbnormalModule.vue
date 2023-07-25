cb098a0f83b740a9c334d6068f3f0c47c263c556
KiriRainCat <2287990465@qq.com>
2023-07-25 11:03:43 +0800

    ✨ feat: 异常登录页完成

--- src/components/auth/AbnormalModule.vue
+++ src/components/auth/AbnormalModule.vue
@@ -3,12 +3,15 @@
     <i-ep-close-bold @click="$router.push({ name: 'auth-index' })" class="ml-4 mt-4 cursor-pointer text-2xl" />
     <div class="text-center">
       <img class="mx-auto mt-1 h-12 w-40 object-cover" src="@/assets/common/GYX_gov.png" />
-      <div class="mt-4 text-3xl font-bold">登录到YXG</div>
+      <div class="mt-4 text-2xl font-bold">输入你的手机号码或YXG ID</div>
+      <div class="mx-[12%] mt-1 text-sm">
+        你的账户存在异常登录活动。为了保证你的账户安全，请输入你的手机号码或YXG ID以确认你的身份。
+      </div>
       <div class="animated-input mx-auto mt-4">
         <input v-model="verification" type="text" required />
-        <label>手机号码、邮箱地址或YXG ID</label>
+        <label>手机号码或YXG ID</label>
       </div>
-      <button class="btn ml-2 mt-8 bg-blue-700 text-base font-bold text-white">下一步</button>
+      <button class="btn ml-2 mt-56 bg-blue-700 text-base font-bold text-white">下一步</button>
     </div>
   </div>
 </template>
